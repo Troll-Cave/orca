cb530abbd4547e1a3dc176cb3e81e8227986e5e1
Tom <whynotavailable@protonmail.com>
2021-12-01 13:59:35 -0500

    what

---
+++ libs/app-catalog/README.md
@@ -0,0 +1,7 @@
+# app-catalog
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test app-catalog` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/app-catalog/src/lib/app-catalog.tsx
@@ -0,0 +1,14 @@
+import './app-catalog.module.scss';
+
+/* eslint-disable-next-line */
+export interface AppCatalogProps {}
+
+export function AppCatalog(props: AppCatalogProps) {
+  return (
+    <div>
+      <h1>Welcome to AppCatalog!</h1>
+    </div>
+  );
+}
+
+export default AppCatalog;
